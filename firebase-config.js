860340316782c75056c0a61a6f5e9777c213a693
AngeloGreco02 <74364163+AngeloGreco02@users.noreply.github.com>
2026-07-11 12:01:02 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.ADHD_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAzXMpG0egCBasZKgD0yDEZVpHHr3Fl84g",
+  authDomain: "progetto-adhd.firebaseapp.com",
+  projectId: "progetto-adhd",
+  storageBucket: "progetto-adhd.firebasestorage.app",
+  messagingSenderId: "855670271341",
+  appId: "1:855670271341:web:94f50a34e67948cada3e64",
+  measurementId: "G-XMH5DLSSLG",
+};
