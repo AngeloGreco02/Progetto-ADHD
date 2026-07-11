08020fa1fd5ebc209971e6eeefa8ca91b230cd3f
AngeloGreco02 <74364163+AngeloGreco02@users.noreply.github.com>
2026-07-11 11:29:51 +0200

    Add files via upload

---
+++ github-upload/firebase-config.js
@@ -0,0 +1,17 @@
+// Incolla qui la configurazione della tua Web App Firebase.
+// La configurazione Firebase Web non è una password: serve al browser per trovare il progetto.
+// Lascia `null` finché non hai creato il progetto Firebase.
+window.ADHD_FIREBASE_CONFIG = null;
+
+/*
+Esempio:
+
+window.ADHD_FIREBASE_CONFIG = {
+  apiKey: "AIza...",
+  authDomain: "progetto-adhd.firebaseapp.com",
+  projectId: "progetto-adhd",
+  storageBucket: "progetto-adhd.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+*/
